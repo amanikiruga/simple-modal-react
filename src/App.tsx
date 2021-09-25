445affd095b5d1163a143fa13b55c17719ce735f
Amani Kiruga <amaniedu71@gmail.com>
2021-09-25 18:54:07 -0400

    modified structure of create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Modal from "./Modal";
+const App = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div>
+            <button onClick={() => setIsOpen(true)}> Open Popup</button>
+            <Modal
+                isOpen={isOpen}
+                onCloseModalBtn={() => {
+                    setIsOpen(false);
+                }}
+            >
+                {" "}
+                I think there is something here?{" "}
+            </Modal>
+        </div>
+    );
+};
 
 export default App;
